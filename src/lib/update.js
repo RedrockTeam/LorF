dc003e9d2f101d8f0eeccb95b5dce8fc6ec2d66f
firk1n <firkin@126.com>
2015-11-23 17:06:51 +0800

    update bug

--- src/lib/update.js
+++ src/lib/update.js
@@ -31,7 +31,7 @@ require(['fastclick','zepto'],function(FastClick,$){
                 return false;
             }
 
-            if(!/1[3|5|7|8][0-9]{9}/.test($('input[name=phonemuber]'))){
+            if(!/1[3|5|7|8][0-9]{9}/.test($('input[name=phonenumber]').val())){
                 alert('电话不对呀');
                 return false;
             }
@@ -41,11 +41,11 @@ require(['fastclick','zepto'],function(FastClick,$){
             data.stu_name = $('input[name=stu-number]').val();
             data.phone = $('input[name=phonenumber]').val();
             $.ajax({
-                type:'POST',
-                url: '',
+                type:'GET',
+                url: 'handleInfo',
                 data:data,
                 success:function(res){
-                    alert(res.status);
+                    alert(res);
                 }
 
             })
