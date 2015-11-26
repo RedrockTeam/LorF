88a6c251adf857883b8398e2e9c15f43c94d437e
刘慧芝 <liuhuizhi@liuhuizhideAir.lan>
2015-11-26 20:28:11 +0800

    release

--- src/lib/release.js
+++ src/lib/release.js
@@ -23,6 +23,42 @@ require(['fastclick','zepto'],function(FastClick,$){
         $("#back").on('click',function(){
             history.back();
         })
+
+
+        $('#release').on('click',function(){
+
+            var data = {};
+            if($("select").val() == '' || $('textarea').val() == '' || $('input[type=place]').val() =='' || $('input[type=date]').val() == ''){
+                alert('请检查 不能为空');
+            }
+
+            //if(!(/1[3|5|7|8][0-9]{9}/.test($('input[name=phone]').val()))){
+            //    alert('电话不对呀');
+            //    return false;
+            //}
+
+            data.kind = $("select").val();
+            data.content = $('textarea').val();
+            data.date = $('input[name=date]').val();
+            data.place = $('input[name=place]').val();
+            data.phone = $('input[name=phone]').val();
+            data.qq = $('input[name=QQ]').val();
+
+            console.log(data);
+            $.ajax({
+                url:'',
+                type:'GET',
+                data:data,
+                dataType:'json',
+                success:function(res){
+                    alert('修改成功');
+                }
+
+            })
+
+        })
+
+
     })
 })
 
