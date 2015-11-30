0d27f3275477d2d5c70ca25bc95640d0d45f2a44
刘慧芝 <liuhuizhi@liuhuizhideAir.lan>
2015-11-30 18:48:09 +0800

    unrelease

--- Application/Home/Controller/UserInfoController.class.php
+++ Application/Home/Controller/UserInfoController.class.php
@@ -13,7 +13,7 @@ class UserInfoController extends CommonController{
     private function _getSelfId(){
 
 //        return session('relace_user_id');
-        return 1;
+        return 11;
     }
 
     public function index(){
