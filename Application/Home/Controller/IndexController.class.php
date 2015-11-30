8630f59f82645671d94234ee5ca40adbaf860948
firk1n <firkin@126.com>
2015-11-30 10:47:13 +0800

    openid

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -14,14 +14,14 @@ class IndexController extends CommonController {
     public function index(){
 
         // 获取到openid并且存到session
-//        $code = I('get.code');
-//
-//        if(!$code){
-//            return $this->redirect($this->oauth2Url);
-//        }
-//
-//        $openId = $this->_getOpenId($code);
-        $openId = 'ouRCyjsp3eo3FJil24fV625V_6no';
+        $code = I('get.code');
+
+        if(!$code){
+            return $this->redirect($this->oauth2Url);
+        }
+
+        $openId = $this->_getOpenId($code);
+//        $openId = 'ouRCyjsp3eo3FJil24fV625V_6no';
         session('openid', $openId);
 
         //判断是否绑定学号, 是否关注重邮小帮手
